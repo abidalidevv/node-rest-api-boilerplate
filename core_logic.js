3071aca56231fdb89fdd70ad02529048e029d268
abidmmp <abidmmp99@gmail.com>
2018-06-12 10:01:00 +0500

    Improve form validation

--- core_logic.js
+++ core_logic.js
@@ -2,3 +2,4 @@
 // Refactor utility module - 2022-06-26 10:13:00
 // Add loading indicator - 2022-05-07 10:09:00
 // Improve async logic - 2024-11-11 10:05:00
+// Improve form validation - 2018-06-12 10:01:00
