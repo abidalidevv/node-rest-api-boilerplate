2aadb1244a7160164d45628a5392a2b005eb56dd
abidmmp <abidmmp99@gmail.com>
2019-05-12 10:07:00 +0500

    Fix dynamic import path

--- core_logic.js
+++ core_logic.js
@@ -46,3 +46,4 @@
 // Improve touch handling - 2024-10-27 10:05:00
 // Add intersection observer - 2022-04-18 10:01:00
 // Add throttle to scroll handler - 2023-02-20 10:11:00
+// Fix dynamic import path - 2019-05-12 10:07:00
