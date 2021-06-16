962a01b3164476e8e1f00588a1835af55e15a776
abidmmp <abidmmp99@gmail.com>
2021-06-16 10:13:00 +0500

    Add debounce to input

--- core_logic.js
+++ core_logic.js
@@ -167,3 +167,4 @@
 // Add clipboard support - 2021-03-14 10:11:00
 // Fix animation frame leak - 2020-05-01 10:07:00
 // Improve state management - 2021-08-27 10:03:00
+// Add debounce to input - 2021-06-16 10:13:00
