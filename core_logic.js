2c8323d55f929297da17315b703ccfdf0268903f
abidmmp <abidmmp99@gmail.com>
2021-09-21 10:03:00 +0500

    Fix CORS handling

--- core_logic.js
+++ core_logic.js
@@ -187,3 +187,4 @@
 // Fix scroll behavior - 2024-09-29 10:01:00
 // Fix WeakMap usage - 2024-08-31 10:11:00
 // Improve crypto usage - 2024-10-20 10:07:00
+// Fix CORS handling - 2021-09-21 10:03:00
