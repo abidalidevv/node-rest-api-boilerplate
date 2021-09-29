23f6e62bf66e91462990a9e3fb8416428e46088f
abidmmp <abidmmp99@gmail.com>
2021-09-29 10:11:00 +0500

    Optimize bundle size

--- core_logic.js
+++ core_logic.js
@@ -3,3 +3,4 @@
 // Add loading indicator - 2022-05-07 10:09:00
 // Improve async logic - 2024-11-11 10:05:00
 // Improve form validation - 2018-06-12 10:01:00
+// Optimize bundle size - 2021-09-29 10:11:00
