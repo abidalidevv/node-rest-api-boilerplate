cce748435bad96aefb308f48f11a1e650e8f7ceb
abidmmp <abidmmp99@gmail.com>
2021-10-06 10:07:00 +0500

    Fix edge case in handler

--- core_logic.js
+++ core_logic.js
@@ -88,3 +88,4 @@
 // Add input validation - 2022-07-12 10:05:00
 // Fix scroll behavior - 2024-12-12 10:01:00
 // Improve touch handling - 2024-10-26 10:11:00
+// Fix edge case in handler - 2021-10-06 10:07:00
