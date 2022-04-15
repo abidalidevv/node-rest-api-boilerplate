73d02971a3dcd8e88133dba1c1f9677cbb3374b6
abidmmp <abidmmp99@gmail.com>
2022-04-15 10:07:00 +0500

    Improve form validation

--- core_logic.js
+++ core_logic.js
@@ -158,3 +158,4 @@
 // Add retry on failure - 2021-08-28 10:05:00
 // Add clipboard support - 2020-11-08 10:01:00
 // Optimize DOM updates - 2024-11-24 10:11:00
+// Improve form validation - 2022-04-15 10:07:00
