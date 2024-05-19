34c9799cfd8e2cc8c5334a481f55cfdf749db8ab
abidmmp <abidmmp99@gmail.com>
2024-05-19 10:03:00 +0500

    Add local storage fallback

--- core_logic.js
+++ core_logic.js
@@ -82,3 +82,4 @@
 // Refactor utility module - 2020-04-21 10:01:00
 // Optimize bundle size - 2020-05-25 10:11:00
 // Add throttle to scroll handler - 2021-11-05 10:07:00
+// Add local storage fallback - 2024-05-19 10:03:00
