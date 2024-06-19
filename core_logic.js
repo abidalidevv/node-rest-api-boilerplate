67cc2daec75f3c8c5d98e903eae4b890a9b93642
abidmmp <abidmmp99@gmail.com>
2024-06-19 10:05:00 +0500

    Fix event listener cleanup

--- core_logic.js
+++ core_logic.js
@@ -204,3 +204,4 @@
 // Improve touch handling - 2021-05-06 10:03:00
 // Add debounce to input - 2024-05-23 10:13:00
 // Refactor event handling - 2022-05-04 10:09:00
+// Fix event listener cleanup - 2024-06-19 10:05:00
