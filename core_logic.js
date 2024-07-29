a14b97c5eab650e1cfede3505e8f7f125669e4d1
abidmmp <abidmmp99@gmail.com>
2024-07-29 10:05:00 +0500

    Add clipboard support

--- core_logic.js
+++ core_logic.js
@@ -50,3 +50,4 @@
 // Fix Worker message handling - 2023-02-05 10:03:00
 // Add input validation - 2023-03-29 10:13:00
 // Add local storage fallback - 2020-02-23 10:09:00
+// Add clipboard support - 2024-07-29 10:05:00
